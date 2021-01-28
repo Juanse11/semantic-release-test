be369a729cfba8440dae448da7d500f92ae140dc
Juan Estrada <jsestrada@uninorte.edu.co>
2021-01-28 15:41:06 -0500

    feat: Add birthday information on home page below name (#6)
    * feat: Wrap name inside a h1 tag
    
    * feat: Add birthday on home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Juan Estrada Vargas
+      <h1>Juan Estrada Vargas</h1>
+      <p>
+        Birthday: 12/11/98
+      </p>
     </div>
   );
 }
